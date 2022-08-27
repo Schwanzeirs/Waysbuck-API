122a6186b1936195cce1b4167679e701b73d99b4
Schwanzeirs <yatamisaki342@gmail.com>
2022-08-27 23:02:32 +0700

    update cloudinary

--- dto/product/product_request.go
+++ dto/product/product_request.go
@@ -4,5 +4,5 @@ type ProductRequest struct {
 	Title  string `json:"title" form:"title" gorm:"type: varchar(255)"`
 	Price  int    `json:"price" form:"price" gorm:"type: int"`
 	UserID int    `json:"user_id" gorm:"type: int"`
-	Image  string `json:"image" form:"image" gorm:"type: varchar(255)"`
+	// Image  string `json:"image" form:"image" gorm:"type: varchar(255)"`
 }
